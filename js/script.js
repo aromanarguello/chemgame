2676531476438cf293030c1a8d8ea9acfd6c5475
Alejandro Roman <aromanarguello@gmail.com>
2017-10-30 15:53:47 -0400

    fixed grid Ni column was hidden, fixed grammar

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ $(document).ready( function () {
   $('.hydrogen').click( function () {
     console.log('CLICK!');
   });
+
+  $('.btn-enter').click( function () {
+    console.log('click bitch');
+  });
 });
