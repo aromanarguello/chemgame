891bd4fa80d91a96b67c47a5ab9618b0ce84f3d8
Alejandro Roman <aromanarguello@gmail.com>
2017-11-01 15:11:35 -0400

    fixed player one, points, and score in navbar.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,21 @@
 $(document).ready( function () {
-
   var playerOne = {
     score: [],
     name: "",
     right: [],
     wrong: []
   };
-  
   var playerTwo = {
     score: [],
     name: "",
     right: [],
     wrong: []
   };
-
   var answer = {};
-
   // find key of currently selected element
   var clickedElement = "";
-
-  // first player points
   var firstPlayerCtx = $('#scoreFirst').html();
-
-  // $('#clickedElementHtml').html(clickedElement);
-
+  var secondPlayerCtx = $('#scoreSecond').html();
 
   $('.img-rounded').click( function (e) {
     // store the inner context from shared class in each HTML and store element symbol value to use as key for object:
@@ -34,30 +26,25 @@ $(document).ready( function () {
     clickedElement = noWhiteSpace;
     console.log(clickedElement);
     $('#clickedElementHtml').html(clickedElement);
-
   });
+
+  // $('#secondPlayer').hide();
 
   $('.btn-enter').click( function () {
     // Store values from modal input for Element name and Atomic Number:
     var enteredElemName = $('#enteredName').val();
     var enteredAtomicNumber = $('#enteredAtomicNum').val();
-
-
     // Add input values to (answer) 'object':
     answer.userElemName = enteredElemName;
     answer.userElemAtomicNumber = enteredAtomicNumber;
-
     // clear input values using .val("")
     $('#enteredName').val("");
     $('#enteredAtomicNum').val("");
-
     // use [clickedElement] to pick the same element that was clicked by player
     console.log(elementAtomic[clickedElement].name);
     console.log(answer.userElemName);
-
     // compare user input against elementAtomic library. If correct, close 'modal', otherwise try again and 'modal' remains open
     if ( answer.userElemName === elementAtomic[clickedElement].name && answer.userElemAtomicNumber === elementAtomic[clickedElement].an) {
-      alert('Correct!');
       $('#myModal').modal('hide');
       var toNum = parseInt(firstPlayerCtx);
       firstPlayerCtx = toNum += 1;
@@ -65,13 +52,11 @@ $(document).ready( function () {
       // After score is updated in HTML push to player object -> score array
       player.score.push(firstPlayerCtx);
 
-
-
       // Re-use and concatenate to disable the clicked element.
       //    |
       $('.' + clickedElement).removeAttr('data-toggle');
                         //        |
-                        //   removes Attrobitue
+                        //   removes Attribitue
       $('.' + clickedElement).removeClass('hovered');
       $('.' + clickedElement).addClass('greyed-out');
 
@@ -85,6 +70,4 @@ $(document).ready( function () {
       $('#myModal').modal('show');
     }
   });
-
-
 });
