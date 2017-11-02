14cda436aebcaa85383d181d8e6b8cd05d465588
Alejandro Roman <aromanarguello@gmail.com>
2017-11-01 23:52:05 -0400

    cleaned js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ $(document).ready( function () {
     wrong: []
   };
   var answer = {};
-  // find key of currently selected element
   var clickedElement = "";
   var firstPlayerCtx = $('#scoreFirst').html();
   var secondPlayerCtx = $('#scoreSecond').html();
@@ -20,18 +19,14 @@ $(document).ready( function () {
   $('.img-rounded').click( function (e) {
     // store the inner context from shared class in each HTML and store element symbol value to use as key for object:
     var targetElement = e.target.textContent;
-    // replace whitespaces created when accessing target textContent
+    // replace whitespaces created when accessing target html()
     var noWhiteSpace = targetElement.replace(/\s/g,'');
-    // Add clicked symbol to 'answer object'
     clickedElement = noWhiteSpace;
     console.log(clickedElement);
     $('#clickedElementHtml').html(clickedElement);
   });
 
-  // $('#secondPlayer').hide();
-
   $('.btn-enter').click( function () {
-    // Store values from modal input for Element name and Atomic Number:
     var enteredElemName = $('#enteredName').val();
     var enteredAtomicNumber = $('#enteredAtomicNum').val();
     // Add input values to (answer) 'object':
@@ -40,23 +35,22 @@ $(document).ready( function () {
     // clear input values using .val("")
     $('#enteredName').val("");
     $('#enteredAtomicNum').val("");
-    // use [clickedElement] to pick the same element that was clicked by player
+
     console.log(elementAtomic[clickedElement].name);
     console.log(answer.userElemName);
-    // compare user input against elementAtomic library. If correct, close 'modal', otherwise try again and 'modal' remains open
-    if ( answer.userElemName === elementAtomic[clickedElement].name && answer.userElemAtomicNumber === elementAtomic[clickedElement].an) {
+
+    if ( answer.userElemName === elementAtomic[clickedElement].name &&
+         answer.userElemAtomicNumber === elementAtomic[clickedElement].an) {
       $('#myModal').modal('hide');
       var toNum = parseInt(firstPlayerCtx);
       firstPlayerCtx = toNum += 1;
       $('#scoreFirst').html(firstPlayerCtx);
-      // After score is updated in HTML push to player object -> score array
-      player.score.push(firstPlayerCtx);
 
-      // Re-use and concatenate to disable the clicked element.
-      //    |
+      playerOne.score.push(firstPlayerCtx);
+      playerOne.right.push(firstPlayerCtx);
+      console.log(playerOne);
+
       $('.' + clickedElement).removeAttr('data-toggle');
-                        //        |
-                        //   removes Attribitue
       $('.' + clickedElement).removeClass('hovered');
       $('.' + clickedElement).addClass('greyed-out');
 
